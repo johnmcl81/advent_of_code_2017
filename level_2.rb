bed0768ab15705671e1de3c2ce56aaee966a2750
johnmcl81 <j.d.mclachlan@gmail.com>
2018-06-23 17:26:39 +0200

    level_2

--- level_2.rb
+++ level_2.rb
@@ -1,5 +1,27 @@
 class Level_2
   def initialize(input)
+    @int_array = input
+  end
 
+  def checksum
+    sum = 0
+    @int_array.each do |row|
+      sum += row.max - row.min
+    end
+    sum
+  end
+
+  def checksum_abs_divides
+    sum = 0
+    @int_array.each do |row|
+      row.sort!.reverse!
+      row.each_with_index do |int, index|
+        (index..row.size - 2).each do |i|
+          division = int.to_f / row[i + 1]
+          sum += division.to_i if (division % 1).zero?
+        end
+      end
+    end
+    sum
   end
 end
--- main.rb
+++ main.rb
@@ -1,10 +1,36 @@
-require 'yaml'
-level_1_input = File.readlines './inputs/level_1_input.txt'
-level_2_input = File.readlines './inputs/level_2_input.txt'
 require './level_1'
+require './level_2'
 
+class AdventOfCode
 
-# --------------------- LEVEL 1 -----------------------
-calculator = Level_1.new(level_1_input)
-puts "Level 1.1: #{calculator.calc_total}"
-puts "Level 1.2: #{calculator.calc_total_with_interval}"
+  def level_1
+    level_1_input = parse_input("level_1_input.txt")
+
+    calculator = Level_1.new(level_1_input)
+    puts "Level 1.1: #{calculator.calc_total}"
+    puts "Level 1.2: #{calculator.calc_total_with_interval}"
+  end
+
+  def level_2
+    level_2_input = parse_input("level_2_input.txt")
+
+    calculator = Level_2.new(level_2_input)
+    puts "Level 2.1: #{calculator.checksum}"
+    puts "Level 2.2: #{calculator.checksum_abs_divides}"
+  end
+
+  private
+
+  def parse_input(filename)
+    parsed_input = []
+    File.readlines("./inputs/#{filename}").each do |line|
+      parsed_input << line.split(' ').map(&:to_i)
+    end
+    parsed_input
+  end
+
+end
+
+AOC = AdventOfCode.new
+AOC.level_1
+AOC.level_2
